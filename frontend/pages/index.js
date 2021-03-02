73e9b7061fcfe52be252719f0660dd9978abd05b
Kris Chamberlin <kris.elliott.chamberlin@gmail.com>
2021-03-02 11:55:49 -0800

    Added basic Date functions to calculate days until wedding.

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,3 +1,18 @@
 export default function HomePage() {
   return <div>Home Page</div>;
+  const today = new Date();
+  const wedding = new Date('October 14, 2021');
+  const weddingString = wedding.toDateString();
+
+  if (today.getMonth() === 9 && today.getDate() > 14) {
+    wedding.setFullYear(wedding.getFullYear() + 1);
+  }
+
+  if (today.getMonth() === 9 && today.getDate() === 14) {
+    return <p>Today's the day!</p>;
+  }
+
+  const oneDay = 1000 * 60 * 60 * 24;
+  const daysLeft = Math.ceil((wedding.getTime() - today.getTime()) / oneDay);
+
 }
