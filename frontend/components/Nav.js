9b12c7ec6f6dfe8cd599f539c624246f82e0a4ac
Kris Chamberlin <kris.elliott.chamberlin@gmail.com>
2021-04-14 10:47:35 -0700

    Added Links for Home, Story, and Photos pages.

--- frontend/components/Nav.js
+++ frontend/components/Nav.js
@@ -3,6 +3,9 @@ import Link from 'next/link';
 export default function Nav() {
   return (
     <nav>
+      <Link href="/">Home</Link>
+      <Link href="/story">Our Story</Link>
+      <Link href="/photos">Photos</Link>
       <Link href="/upload">Upload</Link>
     </nav>
   );
