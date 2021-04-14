31ee98ad849d7e5b85facbbb4aa66d2d051a2172
Kris Chamberlin <kris.elliott.chamberlin@gmail.com>
2021-04-14 10:48:37 -0700

    Imported ApolloProvider and withData, destructured apollo, wrapped Page Component with AP, and created async function for Apollo props.

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,9 +1,26 @@
+import { ApolloProvider } from '@apollo/client';
 import Page from '../components/Page';
+import withData from '../lib/withData';
 
-export default function MyApp({ Component, pageProps }) {
+// ** NOTE: NProgress, Router, etc? Perhaps a sunflower loading component? **
+
+function MyApp({ Component, pageProps, apollo }) {
   return (
-    <Page>
-      <Component {...pageProps} />
-    </Page>
+    <ApolloProvider client={apollo}>
+      <Page>
+        <Component {...pageProps} />
+      </Page>
+    </ApolloProvider>
   );
 }
+
+MyApp.getInitialProps = async function ({ Component, ctx }) {
+  let pageProps = {};
+  if (Component.getInitialProps) {
+    pageProps = await Component.getInitialProps(ctx);
+  }
+  pageProps.query = ctx.query;
+  return { pageProps };
+};
+
+export default withData(MyApp);
